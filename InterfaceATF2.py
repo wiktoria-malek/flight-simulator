fe50ca6b8e0c367fb1b5131cbf7618c1cbd0247b
alatina <alatina@tc-op2.atf-local>
2024-05-20 12:12:11 +0900

    Update

--- InterfaceATF2.py
+++ InterfaceATF2.py
@@ -1,7 +1,15 @@
+import numpy
+import matplotlib.pyplot as pl
+
+from epics import PV, ca
+
 class InterfaceATF2:
 	def __init__(self, nsamples):
         self.nsamples = nsamples;
         self.nbpms = 57;
+        
+        
+        
         
     def get_bpms()
         
