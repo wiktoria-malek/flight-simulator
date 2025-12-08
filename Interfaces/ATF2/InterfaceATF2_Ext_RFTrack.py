f0bfab8522488389847ad5ff5f2cf57bf975b5d0
Andrea Latina <andrea.latina@cern.ch>
2025-12-08 17:37:30 +0900

    Target dispersion work in progress

--- Interfaces/ATF2/InterfaceATF2_Ext_RFTrack.py
+++ Interfaces/ATF2/InterfaceATF2_Ext_RFTrack.py
@@ -10,7 +10,7 @@ class InterfaceATF2_Ext_RFTrack():
 
     def __init__(self, population=2e10, jitter=0.0, bpm_resolution=0.0, nsamples=1):
         self.log = print
-        self.lattice = rft.Lattice('Ext_ATF2/ATF2_EXT_FF_v5.2.twiss')
+        self.lattice = rft.Lattice('Interfaces/ATF2/Ext_ATF2/ATF2_EXT_FF_v5.2.twiss')
         self.lattice.set_bpm_resolution(bpm_resolution)
         for s in self.lattice['*OTR*']:
             screen = rft.Screen()
@@ -131,6 +131,30 @@ class InterfaceATF2_Ext_RFTrack():
 
     def get_elements_position(self,names):
         return [index for index, string in enumerate(self.sequence) if string in names]
+
+    def get_target_dispersion(self, names=None):
+        with open('Ext_ATF2/ATF2_EXT_FF_v5.2.twiss', "r") as file:
+            # lines=file.readlines()
+            lines = [line.strip() for line in file if line.strip()]
+
+        star_symbol = next(i for i, line in enumerate(lines) if line.startswith("*"))
+        dollar_sign = next(i for i, line in enumerate(lines) if line.startswith("$") and i > star_symbol)
+        columns = lines[star_symbol].lstrip("*").split()
+
+        DX_column = columns.index("DX")
+        DY_column = columns.index("DY")
+        elements_names = columns.index("NAME")
+
+        target_disp_x, target_disp_y = [], []
+        for line in lines[dollar_sign + 1:]:
+            data = line.split()
+            bpms_name = data[elements_names].strip('"')
+
+            if names == None or bpms_name in names:
+                target_disp_x.append(float(data[DX_column]))
+                target_disp_y.append(float(data[DY_column]))
+
+        return target_disp_x, target_disp_y
 
     def get_icts(self):
         #print("Reading ict's...")
---
+++ Test files/TestTargetDispersion.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append('..')
+sys.path.append('../Interfaces/ATF2')
+
+from State import State
+
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import os
+
+from InterfaceATF2_Ext_RFTrack import InterfaceATF2_Ext_RFTrack
+I = InterfaceATF2_Ext_RFTrack()
+
+project_name = I.get_name()
+print(f"Selected interface: {project_name}")
+
+# TEST 1 - Read one bpm
+names = I.get_bpms_names()
+mb1x_disp_x, mb1x_disp_y = I.get_target_dispersion(names)
+print('disp x @ MB1X = ', mb1x_disp_x, ' m')
+print('disp y @ MB1X = ', mb1x_disp_y, ' m')
+
+# TEST 2 - Read several bpms
+
+mb1x_disp_x, mb1x_disp_y = I.get_target_dispersion("MB1X")
+print('disp x @ MB1X = ', mb1x_disp_x, ' m')
+print('disp y @ MB1X = ', mb1x_disp_y, ' m')
+
+# TEST 3 - Read all bpms
+target_disp_x, target_disp_y = I.get_target_dispersion()
+
+plt.figure(1)
+plt.plot(target_disp_x, 'b-', linewidth=2, label='disp x')
+plt.plot(target_disp_y, 'r-', linewidth=2, label='disp y')
+plt.legend()
+plt.xlabel('BPM [#]')
+plt.ylabel('target dispersion [m]')
+plt.show()
