f488d5d5895baf3fbbcfe602141f9b3fe3978b62
wmalek <wiktoria.malek@cern.ch>
2025-12-09 12:38:33 +0900

    Regex

--- Test files/test.py
+++ Test files/test.py
@@ -5,6 +5,7 @@ text = "M.1 M.2 M.3"
 result = re.sub(r"M.(\d+)", r"MB\1R", text)
 
 # sed -i 's/old/new/g' file.txt
+#sed -E -i '' 's/M\.([0-9]+)/MB\1R/g' Interfaces/ATF2/DR_ATF2/ATF_DR_lattice.madx
 
 print(result)
 
