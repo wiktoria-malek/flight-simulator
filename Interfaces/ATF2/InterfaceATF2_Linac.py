30a9ede40c4bed03d508d0c35de361a9deeef0d5
wmalek <wiktoria.malek@cern.ch>
2026-03-12 12:32:43 +0100

    Changing laser intensity

--- Interfaces/ATF2/InterfaceATF2_Linac.py
+++ Interfaces/ATF2/InterfaceATF2_Linac.py
@@ -95,9 +95,9 @@ class InterfaceATF2_Linac:
         time.sleep(1)
 
     def change_intensity(self):
-        print(f'Changing laser intensity to {laser_intensity}...')
         new_laser_intensity = 0.15 # 0..1
         laser_intensity = new_laser_intensity * 100 * 5 # Korysko dixit: 100 for percent, 5 convesion factor
+        print(f'Changing laser intensity to {laser_intensity}...')
         PV('RFGun:LaserIntensity1:Write').put(laser_intensity)
         time.sleep(3)
         return self
