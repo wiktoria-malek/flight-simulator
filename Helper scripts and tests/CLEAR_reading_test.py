688d84560a3fc756167775f1c87474e619074276
wmalek <wiktoria.malek@cern.ch>
2026-07-27 10:28:01 +0200

    Debug

--- Helper scripts and tests/CLEAR_reading_test.py
+++ Helper scripts and tests/CLEAR_reading_test.py
@@ -45,109 +45,109 @@ print(f"bact  from method get_quadrupoles: {result['bact']}")
 print("================================================================================")
 
 
-"""Test of one corrector and method get_correctors"""
-
-print("Testing corrector CA.DHG0130, reading its values directly from japc/pyda... ")
-corrector = "CA.DHG0130"
-setting = client.get(f"{corrector}/SettingPPM", context = context_empty).data["current"]
-print(f"{corrector}/SettingPPM#current = {setting}")
-
-acquisition = client.get(f"{corrector}/Acquisition", context = context_acquisition).data["currentAverage"]
-print(f"{corrector}/Acquisition#currentAverage = {acquisition}")
-
-status = client.get(f"{corrector}/Status", context = context_empty).data
-print(f"{corrector}/Status = {status}")
-print("================================================================================")
-print("Testing corrector CA.DHG0130, reading its values directly from interface, using get_correctors method... ")
-
-result = I.get_correctors(names=["CA.DHG0130"])
-print(f"names from method get_correctors: {result['names']}")
-print(f"bdes  from method get_correctors: {result['bdes']}")
-print(f"bact  from method get_correctors: {result['bact']}")
-
-print("================================================================================")
-
-
-"""Test of one screen and method get_screens"""
-
-print("Testing screen CA.BTV0390L, reading its values directly from japc/pyda... ")
-camera = client.get("CA.BTV0390.DigiCam/LastImage", context=context_empty).data
-
-image = camera["image2D"]
-print(f"image from PyDa = {image}")
-
-hpixel = camera["pixelCalSet1"]
-print(f"hpixel from PyDa = {hpixel}")
-
-vpixel = camera["pixelCalSet2"]
-print(f"vpixel from PyDa = {vpixel}")
-
-proj_x = camera["projDataSet1"]
-print(f"proj_x.shape = {proj_x.shape}")
-
-proj_y = camera["projDataSet2"]
-print(f"proj_y.shape = {proj_y.shape}")
-
-x_positions = camera["imagePositionSet1"]
-print(f"x_positions.shape = {x_positions.shape}")
-
-y_positions = camera["imagePositionSet2"]
-print(f"y_positions.shape = {y_positions.shape}")
-
-inout = client.get("CA.BTV0390_CAS.BTV0420/OPSettingSystem1", context = context_empty).data["positionChannel1"]
-print(f"inout from PyDa = {inout}")
-
-print("================================================================================")
-print("Testing screen CA.BTV0390L, reading its values directly from interface, using get_screens method... ")
-result = I.get_screens(names=["CA.BTV0390_CAS.BTV0420"])
-print(f"names from get_screens: {result['names']}")
-print(f"hpixel from get_screens: {result['hpixel']}")
-print(f"vpixel from get_screens: {result['vpixel']}")
-print(f"x from get_screens: {result['x']}")
-print(f"y from get_screens: {result['y']}")
-print(f"sigx from get_screens:  {result['sigx']}")
-print(f"sigy from get_screens:  {result['sigy']}")
-print(f"sum from get_screens: {result['sum']}")
-print(f"images from get_screens: {result['images']}")
-print(f"inout from get_screens: {result['inout']}")
-print("================================================================================")
-
-"""Test of one BPM and method get_bpms"""
-
-print("Testing BPM CA.BPM0530H-SA, reading its values directly from japc/pyda... ")
-bpm = "CA.BPM0530"
-hsamples = client.get(f"{bpm}H-SA/SamplesFromTrigger", context = context_acquisition).data
-print(f"{bpm}H-SA/SamplesFromTrigger = {hsamples}")
-
-vsamples = client.get(f"{bpm}V-SA/SamplesFromTrigger", context = context_acquisition).data
-print(f"{bpm}V-SA/SamplesFromTrigger = {vsamples}")
-
-Ssamples = client.get(f"{bpm}S-SA/SamplesFromTrigger", context = context_acquisition).data
-print(f"{bpm}S-SA/SamplesFromTrigger = {Ssamples}")
-
-h = np.asarray(hsamples["samples"], dtype=float).ravel()
-v = np.asarray(vsamples["samples"], dtype=float).ravel()
-s = np.asarray(Ssamples["samples"], dtype=float).ravel()
-Hpos = np.sum(h)/np.sum(s)
-Vpos = np.sum(v)/np.sum(s)
-
-print("Hpos:", Hpos)
-print("Vpos:", Vpos)
-plt.figure()
-plt.plot(h, label="H")
-plt.plot(v, label="V")
-plt.plot(s, label="S")
-plt.legend()
-plt.show()
-
-print("================================================================================")
-print("Testing BPM CA.BPM0530H-SA, reading its values directly from interface, using get_bpms method... ")
-result = I.get_bpms(names=["CA.BPM0530"])
-print(f"names from get_bpms: {result['names']}")
-print(f"x from get_bpms: {result['x']}")
-print(f"y from get_bpms: {result['y']}")
-print(f"tmit from get_bpms: {result['tmit']}")
-print("================================================================================")
+# """Test of one corrector and method get_correctors"""
+#
+# print("Testing corrector CA.DHG0130, reading its values directly from japc/pyda... ")
+# corrector = "CA.DHG0130"
+# setting = client.get(f"{corrector}/SettingPPM", context = context_empty).data["current"]
+# print(f"{corrector}/SettingPPM#current = {setting}")
+#
+# acquisition = client.get(f"{corrector}/Acquisition", context = context_acquisition).data["currentAverage"]
+# print(f"{corrector}/Acquisition#currentAverage = {acquisition}")
+#
+# status = client.get(f"{corrector}/Status", context = context_empty).data
+# print(f"{corrector}/Status = {status}")
+# print("================================================================================")
+# print("Testing corrector CA.DHG0130, reading its values directly from interface, using get_correctors method... ")
+#
+# result = I.get_correctors(names=["CA.DHG0130"])
+# print(f"names from method get_correctors: {result['names']}")
+# print(f"bdes  from method get_correctors: {result['bdes']}")
+# print(f"bact  from method get_correctors: {result['bact']}")
+#
+# print("================================================================================")
+#
+#
+# """Test of one screen and method get_screens"""
+#
+# print("Testing screen CA.BTV0390L, reading its values directly from japc/pyda... ")
+# camera = client.get("CA.BTV0390.DigiCam/LastImage", context=context_empty).data
+#
+# image = camera["image2D"]
+# print(f"image from PyDa = {image}")
+#
+# hpixel = camera["pixelCalSet1"]
+# print(f"hpixel from PyDa = {hpixel}")
+#
+# vpixel = camera["pixelCalSet2"]
+# print(f"vpixel from PyDa = {vpixel}")
+#
+# proj_x = camera["projDataSet1"]
+# print(f"proj_x.shape = {proj_x.shape}")
+#
+# proj_y = camera["projDataSet2"]
+# print(f"proj_y.shape = {proj_y.shape}")
+#
+# x_positions = camera["imagePositionSet1"]
+# print(f"x_positions.shape = {x_positions.shape}")
+#
+# y_positions = camera["imagePositionSet2"]
+# print(f"y_positions.shape = {y_positions.shape}")
+#
+# inout = client.get("CA.BTV0390_CAS.BTV0420/OPSettingSystem1", context = context_empty).data["positionChannel1"]
+# print(f"inout from PyDa = {inout}")
+#
+# print("================================================================================")
+# print("Testing screen CA.BTV0390L, reading its values directly from interface, using get_screens method... ")
+# result = I.get_screens(names=["CA.BTV0390_CAS.BTV0420"])
+# print(f"names from get_screens: {result['names']}")
+# print(f"hpixel from get_screens: {result['hpixel']}")
+# print(f"vpixel from get_screens: {result['vpixel']}")
+# print(f"x from get_screens: {result['x']}")
+# print(f"y from get_screens: {result['y']}")
+# print(f"sigx from get_screens:  {result['sigx']}")
+# print(f"sigy from get_screens:  {result['sigy']}")
+# print(f"sum from get_screens: {result['sum']}")
+# print(f"images from get_screens: {result['images']}")
+# print(f"inout from get_screens: {result['inout']}")
+# print("================================================================================")
+#
+# """Test of one BPM and method get_bpms"""
+#
+# print("Testing BPM CA.BPM0530H-SA, reading its values directly from japc/pyda... ")
+# bpm = "CA.BPM0530"
+# hsamples = client.get(f"{bpm}H-SA/SamplesFromTrigger", context = context_acquisition).data
+# print(f"{bpm}H-SA/SamplesFromTrigger = {hsamples}")
+#
+# vsamples = client.get(f"{bpm}V-SA/SamplesFromTrigger", context = context_acquisition).data
+# print(f"{bpm}V-SA/SamplesFromTrigger = {vsamples}")
+#
+# Ssamples = client.get(f"{bpm}S-SA/SamplesFromTrigger", context = context_acquisition).data
+# print(f"{bpm}S-SA/SamplesFromTrigger = {Ssamples}")
+#
+# h = np.asarray(hsamples["samples"], dtype=float).ravel()
+# v = np.asarray(vsamples["samples"], dtype=float).ravel()
+# s = np.asarray(Ssamples["samples"], dtype=float).ravel()
+# Hpos = np.sum(h)/np.sum(s)
+# Vpos = np.sum(v)/np.sum(s)
+#
+# print("Hpos:", Hpos)
+# print("Vpos:", Vpos)
+# plt.figure()
+# plt.plot(h, label="H")
+# plt.plot(v, label="V")
+# plt.plot(s, label="S")
+# plt.legend()
+# plt.show()
+#
+# print("================================================================================")
+# print("Testing BPM CA.BPM0530H-SA, reading its values directly from interface, using get_bpms method... ")
+# result = I.get_bpms(names=["CA.BPM0530"])
+# print(f"names from get_bpms: {result['names']}")
+# print(f"x from get_bpms: {result['x']}")
+# print(f"y from get_bpms: {result['y']}")
+# print(f"tmit from get_bpms: {result['tmit']}")
+# print("================================================================================")
 
 
 
