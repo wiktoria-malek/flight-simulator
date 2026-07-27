0d1442d888d9596b082ca5d6c5786f2b8bf62642
wmalek <wiktoria.malek@cern.ch>
2026-07-27 11:22:53 +0200

    Debug

--- Interfaces/CLEAR/InterfaceCLEAR.py
+++ Interfaces/CLEAR/InterfaceCLEAR.py
@@ -404,7 +404,7 @@ class CLEAR_real_machine(AbstractMachineInterface):
         return False
 
     def _wait_for_quadrupole_readback(self, quadrupole, target, tolerance= 5e-3, timeout=10.0, poll_interval=0.05):
-        readback_param = self.quadrupole_get_params[quadrupole]
+        readback_param = self.quad_get_params[quadrupole]
         t0 = time.perf_counter()
         last_value = np.nan
         while time.perf_counter() - t0 < timeout:
