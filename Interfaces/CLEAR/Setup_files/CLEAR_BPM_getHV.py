386ec07fa7dea3c4b68990a0b049408defd3e238
wmalek <wiktoria.malek@cern.ch>
2026-08-27 13:31:52 +0200

    Change BPM program.

--- Interfaces/CLEAR/Setup_files/CLEAR_BPM_getHV.py
+++ Interfaces/CLEAR/Setup_files/CLEAR_BPM_getHV.py
@@ -37,7 +37,7 @@ def threshold_integral(samples, threshold_fraction=0.05):
         end += 1
 
     # Integrate the pulse
-    integral = trapezoid(samples[start:end + 1])
+    integral = trapezoid(samples[start-1:end + 2])
 
     return integral, start, end, peak_idx
 
