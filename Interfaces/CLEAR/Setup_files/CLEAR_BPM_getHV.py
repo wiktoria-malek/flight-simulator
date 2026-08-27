1950afa52a8ded1610e56e877a37f5d50a5bdb56
wmalek <wiktoria.malek@cern.ch>
2026-08-27 18:53:45 +0200

    Invert polarity

--- Interfaces/CLEAR/Setup_files/CLEAR_BPM_getHV.py
+++ Interfaces/CLEAR/Setup_files/CLEAR_BPM_getHV.py
@@ -2,8 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import trapezoid
 
+
 def change_inverted_bpm_polarity(samples, bpm):
-    return np.asarray(samples, dtype=float)
+    samples = np.asarray(samples, dtype=float)
+    bpm = str(bpm)
+    if bpm.startswith("CA."):
+        bpm = bpm[3:]
+    if bpm == "BPM0890":
+        return -samples
+    return samples
+
 
 # Subtract baseline from signal
 def baseline_correct(samples, n_baseline=100):
@@ -11,10 +19,12 @@ def baseline_correct(samples, n_baseline=100):
     corrected_samples = samples - baseline
     return corrected_samples
 
+
 # Peak seek
 def find_peak(samples):
     idx = np.argmax(np.abs(samples))
     return samples[idx], idx
+
 
 # 5% of peak integration threshold region
 def threshold_integral(samples, threshold_fraction=0.05):
@@ -37,17 +47,15 @@ def threshold_integral(samples, threshold_fraction=0.05):
         end += 1
 
     # Integrate the pulse
-    #integral = trapezoid(samples[start-1:end + 2]) # that's the new version
-    integral = trapezoid(samples[start:end + 1]) # that's the old one
+    integral = trapezoid(samples[start:end + 1])
 
     return integral, start, end, peak_idx
 
-def plot_peak(signals, indices, peaks, labels, BPM):
 
+def plot_peak(signals, indices, peaks, labels, BPM):
     fig, ax = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
 
     for axis, signal, idx, peak, label in zip(ax, signals, indices, peaks, labels):
-
         axis.plot(signal, label=f"{label} signal")
         axis.plot(idx, peak, "ro", label=f"Peak = {peak:.2f} mV")
 
@@ -60,7 +68,8 @@ def plot_peak(signals, indices, peaks, labels, BPM):
     plt.tight_layout()
     plt.show()
 
-def plot_integral(signals, integrals, labels, BPM, starts=None,ends=None,):
+
+def plot_integral(signals, integrals, labels, BPM, starts=None, ends=None, ):
     fig, ax = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
 
     for i, (axis, signal, integral, label) in enumerate(zip(ax, signals, integrals, labels)):
@@ -70,12 +79,13 @@ def plot_integral(signals, integrals, labels, BPM, starts=None,ends=None,):
 
         if starts is None:
             # Shade the whole signal
-            axis.fill_between(x, signal, 0, alpha=0.3, label=f"Integral = {integral:.2f}",)
+            axis.fill_between(x, signal, 0, alpha=0.3, label=f"Integral = {integral:.2f}", )
 
         else:
             start = starts[i]
             end = ends[i]
-            axis.fill_between(x[start:end + 1], signal[start:end + 1], 0, alpha=0.3, label=f"Integral = {integral:.2f}",)
+            axis.fill_between(x[start:end + 1], signal[start:end + 1], 0, alpha=0.3,
+                              label=f"Integral = {integral:.2f}", )
             axis.axvline(start, color="green", linestyle="--", label="Start")
             axis.axvline(end, color="red", linestyle="--", label="End")
 
@@ -88,10 +98,11 @@ def plot_integral(signals, integrals, labels, BPM, starts=None,ends=None,):
     plt.tight_layout()
     plt.show()
 
+
 DEFAULT_WINDOW = (260, 360)
 
+
 def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
-    
     """
     Acquire and process the horizontal (H) and vertical (V) BPM signals.
 
@@ -108,7 +119,7 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
             Return the integral of the baseline-corrected signal.
         - "integral_window"
             Return the integral of the baseline-corrected signal over the
-            specified integration window, [min=0, max=730]. If not specified, 
+            specified integration window, [min=0, max=730]. If not specified,
             running with default window.
         - "integral_threshold"
             Return the integral of the baseline-corrected signal between the
@@ -127,8 +138,8 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
 
     H_data = japc.getParam(f"CA.{BPM}H-SA/SamplesFromTrigger")
     V_data = japc.getParam(f"CA.{BPM}V-SA/SamplesFromTrigger")
-    H_samples = np.asarray(H_data["samples"], dtype=float)
-    V_samples = np.asarray(V_data["samples"], dtype=float)
+    H_samples = change_inverted_bpm_polarity(H_data["samples"], BPM)
+    V_samples = change_inverted_bpm_polarity(V_data["samples"], BPM)
 
     H_b_samples = baseline_correct(H_samples)
     V_b_samples = baseline_correct(V_samples)
@@ -138,8 +149,8 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
         H, H_idx = find_peak(H_samples)
         V, V_idx = find_peak(V_samples)
 
-        if plot: # Plot the signals
-                plot_peak([H_samples, V_samples], [H_idx, V_idx], [H, V], ["H", "V"], BPM,)
+        if plot:  # Plot the signals
+            plot_peak([H_samples, V_samples], [H_idx, V_idx], [H, V], ["H", "V"], BPM, )
 
 
     # Find peak on baseline corrected signal (largest magnitude, keeping the sign)
@@ -147,8 +158,8 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
         H, H_idx = find_peak(H_b_samples)
         V, V_idx = find_peak(V_b_samples)
 
-        if plot: # Plot the signals
-            plot_peak([H_b_samples, V_b_samples], [H_idx, V_idx], [H, V], ["H", "V"], BPM,)
+        if plot:  # Plot the signals
+            plot_peak([H_b_samples, V_b_samples], [H_idx, V_idx], [H, V], ["H", "V"], BPM, )
 
 
     # Integration of full BPM signal with baseline correction
@@ -157,7 +168,7 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
         V = trapezoid(V_b_samples)
 
         if plot:
-            plot_integral(signals=[H_b_samples, V_b_samples], integrals=[H, V], labels=["H", "V"], BPM=BPM,)
+            plot_integral(signals=[H_b_samples, V_b_samples], integrals=[H, V], labels=["H", "V"], BPM=BPM, )
 
 
     # Integration of fixed window BPM signal with baseline correction
@@ -167,7 +178,8 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
         V = trapezoid(V_b_samples[window_start:window_end])
 
         if plot:
-            plot_integral(signals=[H_b_samples, V_b_samples], integrals=[H, V], labels=["H", "V"], BPM=BPM, starts=[window_start, window_start], ends=[window_end, window_end],)
+            plot_integral(signals=[H_b_samples, V_b_samples], integrals=[H, V], labels=["H", "V"], BPM=BPM,
+                          starts=[window_start, window_start], ends=[window_end, window_end], )
 
     # Integration between 5% of the peak threshold region with baseline correction
     elif mode == "integral_threshold":
@@ -175,15 +187,17 @@ def get_bpm_hv(BPM, mode, plot=False, window=DEFAULT_WINDOW):
         V, V_start, V_end, V_peak_idx = threshold_integral(V_b_samples)
 
         if plot:
-            plot_integral(signals=[H_b_samples, V_b_samples], integrals=[H, V], labels=["H", "V"], BPM=BPM, starts=[H_start, V_start], ends=[H_end, V_end],)
+            plot_integral(signals=[H_b_samples, V_b_samples], integrals=[H, V], labels=["H", "V"], BPM=BPM,
+                          starts=[H_start, V_start], ends=[H_end, V_end], )
 
     else:
-        raise ValueError(f"Unknown mode '{mode}'. " "Choose from: 'peak', 'baseline_peak', " "'integral', 'integral_window', 'integral_threshold'.")
+        raise ValueError(
+            f"Unknown mode '{mode}'. " "Choose from: 'peak', 'baseline_peak', " "'integral', 'integral_window', 'integral_threshold'.")
 
-    return H, V #S
+    return H, V  # S
+
 
 def process_bpm_signal(samples, mode, window=DEFAULT_WINDOW):
-
     if mode == "peak":
         value, _ = find_peak(samples)
 
